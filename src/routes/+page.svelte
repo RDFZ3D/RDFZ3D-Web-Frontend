e5c7836059ca3618f71ae56b6beaa72088466d58
rogerwang2008 <rogerwang2008@163.com>
2025-06-23 16:22:40 +0800

    修复 主页小屏幕不友好

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,15 +20,17 @@
 </script>
 
 <div
-  class="flex min-h-[calc(100vh_-_64px)] flex-col items-center justify-center bg-cover bg-center"
+  class="flex min-h-[calc(100vh_-_64px)] flex-col items-center justify-center bg-cover bg-center text-center"
   style="background-image: url({isMobile ? '/bg-mobile.png' : '/bg.png'});"
 >
-  <div class="card bg-neutral/40 text-neutral-content items-center p-16 shadow-xl backdrop-blur-sm">
+  <div
+    class="card bg-neutral/40 text-neutral-content items-center p-6 shadow-xl backdrop-blur-sm md:p-16"
+  >
     <h1 class="mb-5 text-5xl font-extrabold drop-shadow-lg md:text-6xl">{$LL.terms.r3d_app()}</h1>
     <h2 class="mb-6 text-2xl font-bold drop-shadow-lg md:text-3xl">
       {$LL.terms.r3d_project()}
     </h2>
-    <p class="mb-8 max-w-xl text-center text-lg whitespace-pre-line drop-shadow">
+    <p class="mb-8 max-w-xl text-lg whitespace-pre-line drop-shadow">
       {$LL.descriptions.index()}
     </p>
     <div class="mb-6 flex gap-4">
@@ -37,7 +39,10 @@
       >
     </div>
     <div class="flex w-full justify-center">
-      <a href="/user/login" class="link link-hover opacity-70"> 登录 / 注册 </a>
+      <a href="/user/login" class="link link-hover opacity-70">
+        {$LL.user.functions.login.title()}
+        / {$LL.user.functions.register.title()}
+      </a>
     </div>
   </div>
 </div>
