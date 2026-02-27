2c1fe8e3ab7eafe38d96f5f8ccc22255c6793b2d
rogerwang2008 <rogerwang2008@163.com>
2026-02-27 20:35:58 +0800

    为 /user/profile 添加一点上下间距

--- src/routes/user/profile/+layout.svelte
+++ src/routes/user/profile/+layout.svelte
@@ -4,7 +4,7 @@
   let { children } = $props();
 </script>
 
-<div class="mx-auto flex max-w-4xl flex-col gap-4 md:flex-row">
+<div class="mx-auto my-3 flex max-w-4xl flex-col gap-4 md:flex-row">
   <ul
     class="menu menu-horizontal md:menu-vertical bg-base-200 items-center self-start md:basis-64 md:items-stretch"
   >
@@ -13,7 +13,7 @@
     <li><a href="avatar">{$LL.user.functions.profile.avatar.title()}</a></li>
     <li><a href="auth">{$LL.user.functions.profile.auth_info.title()}</a></li>
   </ul>
-  <div class="flex-grow">
+  <div class="grow">
     {@render children()}
   </div>
 </div>
